d26e8e69c86067eb5cbee78b8ba71866f82c40ac
Benedek Burgess <benedekb97@gmail.com>
2018-06-27 21:02:18 +0200

    Add error reporting to env

--- bootstrap/init.php
+++ bootstrap/init.php
@@ -8,6 +8,14 @@ session_start();
 
 require_once 'functions.php';
 
+if(env('DEBUG_MODE') == "PRODUCTION") {
+    error_reporting(E_ERROR);
+}elseif(env('DEBUG_MODE') == "DEBUG") {
+    error_reporting(E_STRICT);
+}else{
+    error_reporting(E_ALL);
+}
+
 Base::db_connect(env('MYSQL_HOST'), env('MYSQL_USER'), env('MYSQL_PASSWORD'), env('MYSQL_DATABASE'));
 
 Request::create();
